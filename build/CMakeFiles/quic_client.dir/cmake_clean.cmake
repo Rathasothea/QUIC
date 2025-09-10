91bc36c65b3a64df8feb7aa04463dac9a164e9f7
kali <kali@kali.kali>
2025-09-10 03:33:27 -0400

    Add build folder

---
+++ build/CMakeFiles/quic_client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quic_client.dir/src/client.c.o"
+  "CMakeFiles/quic_client.dir/src/client.c.o.d"
+  "bin/quic_client"
+  "bin/quic_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/quic_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/quic_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quic_server.dir/src/server.c.o"
+  "CMakeFiles/quic_server.dir/src/server.c.o.d"
+  "bin/quic_server"
+  "bin/quic_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/quic_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
